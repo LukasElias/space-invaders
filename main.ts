67f408c0b783369723d5af69da8187ffd498d2f3
Lukas Elias Lund Majland <155237879+LukasElias@users.noreply.github.com>
2026-04-14 13:46:33 +0000

    Spawned a player sprite

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
 // Setup
 
+info.setLife(5)
+scene.setBackgroundImage(assets.image`world`)
+
 // Spawn player
+
+let player = sprites.create(assets.image`player`, SpriteKind.Player)
+
+player.setPosition(
+    scene.screenWidth() / 2,
+    scene.screenHeight() / 8 * 7,
+)
 
 // Spawn enemies
 
-// Spawn 
+function spawn_enemy_block(width: number, height: number) {
+
+}
