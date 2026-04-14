d611d756bae2b6056fc405cdd443504296b4ba33
Lukas Elias Lund Majland <155237879+LukasElias@users.noreply.github.com>
2026-04-14 14:43:35 +0000

    Idk if I should make this a class or function.

--- main.ts
+++ main.ts
@@ -14,6 +14,36 @@ player.setPosition(
 
 // Spawn enemies
 
-function spawn_enemy_block(width: number, height: number) {
+const ENEMY_SIZE = 16;
 
+class EnemyGroup {
+    x: number
+    y: number
+    width: number
+    height: number
+
+    constructor(x: number, y: number, width: number, height: number) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+    }
+
+    spawn_enemies() {
+        for (let enemy_x = 0; enemy_x < this.width; enemy_x++) {
+            let pos_x = this.x + enemy_x * ENEMY_SIZE
+
+            for (let enemy_y = 0; enemy_y < this.height; enemy_y++) {
+                let pos_y = this.y + enemy_y * ENEMY_SIZE
+
+                let enemy = sprites.create(assets.image`enemy`, SpriteKind.Enemy)
+
+                enemy.setPosition(pos_x, pos_y)
+            }
+        }
+    }
 }
+
+let enemy_group = new EnemyGroup(0, 40, 10, 4)
+
+enemy_group.spawn_enemies()
